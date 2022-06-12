46d7291f6acf9e51e9e90159971b0b8d780e42e1
oku-053 <r.oku@hajimari.inc>
2022-06-13 02:13:33 +0900

    プロフィール画面ツイート一覧表示機能最低限実装

--- app/resources/views/users/show.blade.php
+++ app/resources/views/users/show.blade.php
@@ -62,14 +62,15 @@
         </div>
         @if (isset($timelines))
             @foreach ($timelines as $timeline)
+                {{-- <?php dd($timeline->user_id) ?> --}}
                 <div class="col-md-8 mb-3">
                     <div class="card">
                         <div class="card-haeder p-3 w-100 d-flex">
                             <img src="{{ asset('storage/profile_image/' .$user->profile_image) }}" class="rounded-circle" width="50" height="50">
-                            {{-- <div class="ml-2 d-flex flex-column flex-grow-1">
-                                <p class="mb-0">{{ $timeline->user->name }}</p>
-                                <a href="{{ url('users/' .$timeline->user->user_id) }}" class="text-secondary">{{ $timeline->user->user_id }}</a>
-                            </div> --}}
+                            <div class="ml-2 d-flex flex-column flex-grow-1">
+                                <p class="mb-0">{{ $user->name }}</p>
+                                <a href="{{ url('users/' .$timeline->user_id) }}" class="text-secondary">{{ $timeline->user_id }}</a>
+                            </div>
                             <div class="d-flex justify-content-end flex-grow-1">
                                 <p class="mb-0 text-secondary">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
                             </div>
@@ -77,24 +78,6 @@
                         <div class="card-body">
                             {{ $timeline->text }}
                         </div>
-                        {{-- <div class="card-footer py-1 d-flex justify-content-end bg-white">
-                            @if ($timeline->user->user_id === Auth::user()->user_id)
-                                <div class="dropdown mr-3 d-flex align-items-center">
-                                    <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v fa-fw"></i>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                        <form method="POST" action="{{ url('tweets/' .$timeline->id) }}" class="mb-0">
-                                            @csrf
-                                            @method('DELETE')
-
-                                            <a href="{{ url('tweets/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
-                                            <button type="submit" class="dropdown-item del-btn">削除</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            @endif --}}
-                        </div> 
                     </div>
                 </div>
             @endforeach
