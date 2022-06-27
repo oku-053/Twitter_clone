240e51f1695409dca661aaa19713a74d1e52ffde
oku-053 <r.oku@hajimari.inc>
2022-06-27 16:22:11 +0900

    PRコメント修正

--- app/resources/js/tweet.js
+++ app/resources/js/tweet.js
@@ -1,18 +1,18 @@
 //文字数カウント
 var formInput = document.getElementById("tweetForm");
 var counterSpan = document.getElementById("inputCounter");
-class_danger = "mb-4 text-danger"
-class_default =  document.getElementById("inputCount").className
+classDanger = "mb-4 text-danger"
+classDefault =  document.getElementById("inputCount").className
 
 formInput.addEventListener("keyup", function() {
   counterSpan.textContent = formInput.value.length;
   switch(true){
     case counterSpan.textContent > 140:
-        document.getElementById("inputCount").className = class_danger;
+        document.getElementById("inputCount").className = classDanger;
         document.getElementById("tweetButton").disabled = true;
         break;
     default:
-        document.getElementById("inputCount").className = class_default;
+        document.getElementById("inputCount").className = classDefault;
         document.getElementById("tweetButton").disabled = false;
   }
 });
