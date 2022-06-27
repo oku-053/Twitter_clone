80bfab00e57873b96e365ac13c779269e5487b42
oku-053 <r.oku@hajimari.inc>
2022-06-27 17:43:11 +0900

    PRコメント修正

--- app/app/Http/Controllers/TweetsController.php
+++ app/app/Http/Controllers/TweetsController.php
@@ -9,26 +9,14 @@ use Illuminate\Support\Facades\Validator;
 class TweetsController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        $user = auth()->user();
-
         return view('tweets.create', [
-            'user' => $user
+            'user' => auth()->user()
         ]);
     }
 
@@ -36,6 +24,7 @@ class TweetsController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param Tweet $tweet
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request, Tweet $tweet)
@@ -50,50 +39,5 @@ class TweetsController extends Controller
         $tweet->tweetStore($user->user_id, $requestData);
 
         return back()->with('flash_message', 'Tweeting is complete!');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
