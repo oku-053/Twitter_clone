2810ab35ec04943ee7a03f298177c2123d4e2b08
EC2 Default User <ec2-user@ip-10-0-0-250.ap-northeast-1.compute.internal>
2022-07-14 07:52:40 +0000

    ツイートボタン修正

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -105,8 +105,7 @@
                 </a>
             </li>
             <li>
-                <a class="btn btn-md btn-primary" href="{{ route('tweets.create') }}">
-                    <svg class="bi me-2" width="16" height="16"></svg>
+                <a class="btn btn-md btn-primary-tweet" href="{{ route('tweets.create') }}">
                     {{ __('Tweet') }}
                 </a>
             </li>
